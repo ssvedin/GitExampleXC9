41fbd3c0fa3bdfa0e1ff27288def694e9c00b78d
Sabrina Svedin <sabrina.svedin@gmail.com>
2018-06-27 09:51:54 -0700

    New file to test custom header

---
+++ GitExampleXC9/TestingCustomHeader.swift
@@ -0,0 +1,13 @@
+//// 
+// TestingCustomHeader.swift
+// GitExampleXC9
+// 
+// Created by Sabrina Svedin on 6/27/18.
+// Copyright © 2018 Sabrina Svedin. All rights reserved.
+// 
+
+import Cocoa
+
+class TestingCustomHeader: NSObject {
+
+}
